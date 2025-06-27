6b51ebe06b8ee45823edf9ec1791c6f7cc375d68
ouhare <44320776+ouharetaso@users.noreply.github.com>
2025-06-27 10:03:22 +0900

    change signature in Parser trait to improve combinator reusability; add or_else, zero_or_more, one_or_more, char_range and related tests

--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,13 @@
-
-pub trait Parser{
+pub trait Parser: Clone{
     type Output;
 
     fn run<'i>(&self, input: &'i str) -> Option<(Self::Output, &'i str)>;
 
-    fn map<B>(&self, f: impl Fn(Self::Output) -> B) -> impl Parser<Output = B> {
+    fn map<B, F>(self, f: F) -> impl Parser<Output = B>
+    where
+        Self: Sized + Clone,
+        F: Fn(Self::Output) -> B + Clone
+    {
         p_fn(move|input| {
             match self.run(input) {
                 Some((a, rest)) => Some((f(a), rest)),
@@ -13,14 +16,32 @@ pub trait Parser{
         })
     }
 
-    fn and_then<B, F>(&self, f: impl Fn(Self::Output) -> F) -> impl Parser<Output = B>
+    fn and_then<B, P, F>(self, f: F) -> impl Parser<Output = B>
     where
-        F: Parser<Output = B>
+        Self: Sized + Clone,
+        P: Parser<Output = B>,
+        F: Fn(Self::Output) -> P + Clone
     {
+        let parser = self.clone();
         p_fn(move|input| {
-            match self.run(input) {
+            match parser.run(input) {
                 Some((a, rest)) => f(a).run(rest),
                 None => None
+            }
+        })
+    }
+
+    fn or_else<P>(self, p: P) -> impl Parser<Output = Self::Output>
+    where
+        Self: Sized + Clone,
+        P: Parser<Output = Self::Output>
+    {
+        let parser = p.clone();
+        let _self = self.clone();
+        p_fn(move|input| {
+            match _self.run(input) {
+                res @ Some(_) => res,
+                None => parser.run(input)
             }
         })
     }
@@ -28,7 +49,7 @@ pub trait Parser{
 
 fn p_fn<F, A>(f: F) -> FnParser<F, A>
 where
-    for<'i> F: Fn(&'i str) -> Option<(A, &'i str)>
+    for<'i> F: Fn(&'i str) -> Option<(A, &'i str)> + Clone
 {
     FnParser{
         f,
@@ -38,7 +59,7 @@ where
 
 impl<A, F> Parser for F
 where
-    F: for<'i> Fn(&'i str) -> Option<(A, &'i str)>
+    F: for<'i> Fn(&'i str) -> Option<(A, &'i str)> + Clone
 {
     type Output = A;
     fn run<'i>(&self, input: &'i str) -> Option<(Self::Output, &'i str)> {
@@ -46,14 +67,27 @@ where
     }
 }
 
-struct FnParser<F, A> {
+#[derive(Copy)]
+struct FnParser<F, A>
+where
+    for<'i> F: Fn(&'i str) -> Option<(A, &'i str)> + Clone
+{
     f: F,
     _phantom: std::marker::PhantomData<A>
 }
 
+impl<F, A> Clone for FnParser<F, A>
+where
+    for<'i> F: Fn(&'i str) -> Option<(A, &'i str)> + Clone
+{
+    fn clone(&self) -> Self {
+        FnParser { f: self.f.clone(), _phantom: std::marker::PhantomData }
+    }
+}
+
 impl<F, A> Parser for FnParser<F, A>
 where
-    for<'i> F: Fn(&'i str) -> Option<(A, &'i str)>
+    for<'i> F: Fn(&'i str) -> Option<(A, &'i str)> + Clone
 {
     type Output = A;
 
@@ -84,6 +118,56 @@ pub fn any_char() -> impl Parser<Output = char> {
     })
 }
 
+pub fn zero_or_more<A>(p: impl Parser<Output = A>) -> impl Parser<Output = Vec<A>> {
+    p_fn(move |input| {
+        let mut input = input;
+        let mut vec = Vec::new();
+        loop {
+            match p.run(input) {
+                Some((a, rest)) => {
+                    vec.push(a);
+                    input = rest;
+                },
+                None => break Some((vec, input))
+            }
+        }
+    })
+}
+
+pub fn one_or_more<A>(p: impl Parser<Output = A>) -> impl Parser<Output = Vec<A>> {
+    p_fn(move |input| {
+        let mut input = input;
+        let mut vec = Vec::new();
+        match p.run(input) {
+                Some((a, rest)) => {
+                    vec.push(a);
+                    input = rest;
+                },
+                None => return None
+        }
+        loop {
+            match p.run(input) {
+                Some((a, rest)) => {
+                    vec.push(a);
+                    input = rest;
+                },
+                None => break Some((vec, input))
+            }
+        }
+    })
+}
+
+pub fn char_range(range: impl std::ops::RangeBounds<char> + Clone) -> impl Parser<Output = char> {
+    p_fn(move|input| {
+        if let Some(c) =  input.chars().next() {
+            if range.contains(&c) {
+                return Some((c, &input[c.len_utf8()..]));
+            }
+        }
+        None
+    })
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -100,7 +184,7 @@ mod tests {
     #[test]
     fn test_and_then_char() {
         let parse_a = char1('a');
-        let parse_ab = parse_a.and_then(|_| char1('b'));
+        let parse_ab = parse_a.clone().and_then(|_| char1('b'));
 
         assert_eq!(parse_ab.run("abcd"), Some(('b', "cd")));
         assert_eq!(parse_ab.run("bcde"), None);
@@ -109,9 +193,67 @@ mod tests {
     #[test]
     fn test_any_char() {
         let parse_any_char = any_char();
-        let parse_any_2chars = parse_any_char.and_then(|_| any_char());
+        let parse_any_2chars = parse_any_char.clone().and_then(|_| any_char());
 
         assert_eq!(parse_any_char.run("hello"), Some(('h', "ello")));
         assert_eq!(parse_any_2chars.run("hello"), Some(('e', "llo")));
+    }
+
+    #[test]
+    fn test_or_else() {
+        let parse_a = char1('a');
+        let parse_b = char1('b');
+
+        let parse_a_or_b = parse_a.or_else(parse_b);
+
+        assert_eq!(parse_a_or_b.run("abc"), Some(('a', "bc")));
+        assert_eq!(parse_a_or_b.run("bcd"), Some(('b', "cd")));
+        assert_eq!(parse_a_or_b.run("cde"), None);
+        assert_eq!(parse_a_or_b.run("cde"), None);
+    }
+
+    #[test]
+    fn test_zero_or_more() {
+        let parse_a = char1('a');
+        let parse_repeat_a = zero_or_more(parse_a);
+
+        assert_eq!(parse_repeat_a.run("abc"), Some((vec!['a'], "bc")));
+        assert_eq!(parse_repeat_a.run("aaaabc"), Some((vec!['a', 'a', 'a', 'a'], "bc")));
+        assert_eq!(parse_repeat_a.run("bc"), Some((vec![], "bc")));        
+    }
+
+    #[test]
+    fn test_one_or_more() {
+        let parse_a = char1('a');
+        let parse_repeat_one_more_a = one_or_more(parse_a);
+
+        assert_eq!(parse_repeat_one_more_a.run("abc"), Some((vec!['a'], "bc")));
+        assert_eq!(parse_repeat_one_more_a.run("aaaabc"), Some((vec!['a', 'a', 'a', 'a'], "bc")));
+        assert_eq!(parse_repeat_one_more_a.run("bc"), None);        
+    }
+
+    #[test]
+    fn test_char_range() {
+        let lower_alphabet = char_range('a'..='z');
+        let upper_alphabet = char_range('A'..='Z');
+        let alphabet = lower_alphabet.clone().or_else(upper_alphabet);
+        let number = char_range('0'..='9');
+        let alpha_numeral = alphabet.clone().or_else(number);
+        let underscore = char1('_');
+
+        let identifier_first = underscore.clone().or_else(alphabet);
+        let identifier_rest = underscore.clone().or_else(alpha_numeral);
+
+        let identifier = identifier_first.clone().and_then(move|first| {
+            zero_or_more(identifier_rest.clone()).map(move |mut rest| {
+                let mut chars = vec![first];
+                chars.append(&mut rest);
+                chars.into_iter().collect::<String>()
+            })
+        });
+
+        assert_eq!(identifier.run("id123  "), Some(("id123".to_string(), "  ")));
+        assert_eq!(identifier.run("_start;"), Some(("_start".to_string(), ";")));
+        assert_eq!(identifier.run("8192u16"), None);
     }
 }
